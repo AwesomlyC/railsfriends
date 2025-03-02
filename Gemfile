934414d1950ac999b2eeff79bb06b302c121ed0a
Victor Chhun <109203183+AwesomlyC@users.noreply.github.com>
2025-03-01 18:56:53 -0800

    added another association and finalize friend application.

--- Gemfile
+++ Gemfile
@@ -9,7 +9,6 @@ gem "turbo-rails"
 gem "stimulus-rails"
 gem "jbuilder"
 gem 'devise', '~> 4.9', '>= 4.9.4'
-
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 
 gem "solid_cache"
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,28 @@ class HomeController < ApplicationController
   end
 
   def about
+    @about_me = "Lorem ipsum odor amet, consectetuer adipiscing elit. Varius fames turpis iaculis; conubia rhoncus parturient neque accumsan. 
+    Eget scelerisque ad nascetur mus, vel auctor. Curabitur augue mollis mattis lacinia porta dictumst primis? Suscipit inceptos accumsan primis 
+    tortor lobortis convallis mauris libero scelerisque. Litora sem rutrum senectus, natoque nullam dolor elit. Vestibulum cubilia integer nulla 
+    efficitur mi tristique. Nec sagittis parturient at a dignissim. Cursus finibus pellentesque dictumst luctus luctus duis tellus.
+
+    Interdum ultrices pharetra nisl platea parturient iaculis! Dignissim platea justo pellentesque justo consequat curabitur eleifend felis.
+     Luctus nunc quam hendrerit curabitur congue tempor. Gravida lectus felis suscipit ad curabitur nibh nisi tempor. Mollis efficitur aenean urna 
+     condimentum imperdiet velit augue dui cubilia. Fringilla mus odio ligula natoque interdum dolor magnis. Adipiscing blandit mus non at condimentum.
+      Netus cursus suscipit orci at gravida integer ullamcorper.
+
+    Diam nascetur porttitor quam elit curabitur dictum dolor neque. Hac purus tincidunt mollis vitae vestibulum laoreet. Aelit phasellus ipsum integer bibendum?
+     Nunc et donec curae orci vel venenatis justo. Tortor class orci volutpat vitae morbi consequat. Et non varius velit platea aenean etiam id inceptos adipiscing.
+
+    Condimentum mauris duis lobortis tempus ligula ligula. Et bibendum mauris dictum fringilla pellentesque habitasse montes. 
+    Ligula habitant integer eu ornare nunc conubia justo. Mus nam dapibus suscipit auctor habitasse ligula congue. 
+    Consectetur litora dictum pulvinar tristique non pharetra praesent iaculis. Mauris blandit magnis blandit commodo tempor suscipit dis. 
+    Sed ac sodales congue cubilia mattis facilisis volutpat vestibulum netus. Fames conubia gravida nam natoque nam erat at consequat.
+     Vivamus arcu convallis neque sollicitudin donec dui netus tempor. In egestas fringilla senectus vulputate ornare facilisis suscipit.
+
+    Integer taciti ad sodales laoreet, fusce praesent vitae maecenas nunc. Potenti iaculis augue leo, metus diam convallis metus. 
+    Dignissim mollis eros cursus est erat convallis varius. Sodales ad montes dictum finibus habitasse hac ipsum eleifend. 
+    Turpis integer quis; fringilla ac potenti at nulla. Viverra auctor ad nam sagittis libero aliquet sollicitudin.
+     Donec laoreet montes cras consequat congue justo."
   end
 end
